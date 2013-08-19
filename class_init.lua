5c468add13b5e25692759e36c8643065f4da60d9
Nick ten Veen <nveen0930@gmail.com>
2013-08-20 00:05:17 +0200

    bugfix

--- class_init.lua
+++ class_init.lua
@@ -5,7 +5,7 @@ local url_repo = "https://github.com/besuikerd/computercraft-classes/blob/master
 
 local files = {
 	{name = "settings", path = "class_settings", file = "class_settings.lua"},
-	{name = "settings", path = "class_settings", file = "class_settings.lua"},
+	{name = "class", path = "class", file = "class.lua"},
 	{name = "settings", path = "class_settings", file = "class_settings.lua"},
 }
 
