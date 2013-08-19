1750c7cb186a00f5121b10df88f3f76704803f5c
Nick ten Veen <nveen0930@gmail.com>
2013-08-19 23:55:27 +0200

    created init script

---
+++ class_init.lua
@@ -0,0 +1,25 @@
+local path_settings = "class_settings
+local path_api = "class"
+
+local url_repo = "https://github.com/besuikerd/computercraft-classes/blob/master/
+
+local files = {
+	{name = "settings", path = "class_settings", file = "class_settings.lua"},
+	{name = "settings", path = "class_settings", file = "class_settings.lua"},
+	{name = "settings", path = "class_settings", file = "class_settings.lua"},
+	
+}
+
+--load necessary files
+for i, file in ipairs(files) do
+	io.write(string.format("downloading %s...", file.name))
+	local handle = fs.open(file.path, "w")
+	local url = string.format("%s/%s", url_repo, file.file)
+	local request = http.get(url)
+	if not request then error(string.format("%s not found"), url)
+	handle.write(request.readAll())
+	handle.close()
+	print(" done!")
+end
+
+
---
+++ class_settings.lua
@@ -0,0 +1,11 @@
+__class_settings = {
+	
+	--url of the repo
+	url = "https://github.com/besuikerd",
+	
+	--name of the repo
+	repo = "computercraft-classes",
+	
+	--
+	branch = "raw/master
+}
