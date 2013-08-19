237cc1f8e6d1adf5806dcbeca2ee36d7c3314f65
Nick ten Veen <nveen0930@gmail.com>
2013-08-20 00:41:34 +0200

    .

--- class_init.lua
+++ class_init.lua
@@ -1,7 +1,7 @@
 local path_settings = "class_settings"
 local path_api = "class"
 
-local url_repo = "https://github.com/besuikerd/computercraft-classes/raw/master/"
+local url_repo = "https://raw.github.com/besuikerd/computercraft-classes/master/"
 
 local files = {
 	{name = "settings", path = "class_settings", file = "class_settings.lua"},
